1bf5c4b43020b31f3f36c3a7b56a2329697b6d81
johnmanjohnston <c.ethanjohn@gmail.com>
2023-03-23 18:03:43 +0400

    UI changes, file opening and file saving

--- txteditor/MainWindow.xaml.cs
+++ txteditor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,24 +13,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace txteditor
 {
     public partial class MainWindow : Window
     {
+        private string? currentFilePath = null;
+
         public MainWindow()
         {
             InitializeComponent();
             this.SizeChanged += WindowSizeChanged;
             this.textboxmain.TextChanged += TextboxChange;
             this.KeyDown += OnKeyDown;
+
+            this.savebtn.Click += Save;
+            this.saveasbtn.Click += SaveAs;
+            this.openbtn.Click += Open;
         }
 
         public void TextboxChange(object sender, TextChangedEventArgs e)
         {
             if (linecounterlabel != null && textboxmain != null)
             {
-                linecounterlabel.Text = textboxmain.LineCount.ToString() + " Line(s)";
+                if (currentFilePath == null) 
+                {
+                    savelabel.Text = "Unsaved Changes";
+                }
+
+                int lCount = textboxmain.LineCount;
+
+                linecounterlabel.Text = $"{lCount} Line";
+                if (lCount != 1) { linecounterlabel.Text += "s"; }
+
+                savelabel.Text = "Unsaved Changes";
             }
         }
 
@@ -54,5 +72,37 @@ namespace txteditor
                 }
             }
         }
+
+        private void Save(object sender, EventArgs e) 
+        {
+            if (currentFilePath == null) 
+            {
+                MessageBox.Show("No file loaded :/");
+                return;
+            }
+
+            File.WriteAllText(currentFilePath, textboxmain.Text);
+            savelabel.Text = "Saved";
+        }
+
+        private void SaveAs(object sender, EventArgs e) 
+        {
+        }
+
+        private void Open(object sender, EventArgs e) 
+        {
+            var dialog = new OpenFileDialog();
+
+            bool? res = dialog.ShowDialog();
+
+            if (res == true) 
+            {
+                string filePath = dialog.FileName;
+                textboxmain.Text = File.ReadAllText(filePath);
+                currentFilePath = filePath;
+                savelabel.Text = "Saved";
+            }
+        }
     }
 }
+
