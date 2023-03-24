1c189353228931670857f09b49ab603d30cf9564
johnmanjohnston <c.ethanjohn@gmail.com>
2023-03-24 19:44:50 +0400

    Added Save As feature

--- txteditor/MainWindow.xaml.cs
+++ txteditor/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -29,18 +29,22 @@ namespace txteditor
 
             saved = false;
 
-            this.SizeChanged += WindowSizeChanged;
-            this.textboxmain.TextChanged += TextboxChange;
-            this.KeyDown += OnKeyDown;
-            this.KeyDown += TextboxScroll;
-            this.MouseWheel += TextboxScroll;
 
-            this.savebtn.Click += Save;
+
+            this.SizeChanged             += WindowSizeChanged;
+            this.textboxmain.TextChanged += TextboxChange;
+            this.KeyDown                 += OnKeyDown;
+            this.KeyDown                 += TextboxScroll;
+            this.MouseWheel              += TextboxScroll;
+
+            this.savebtn.Click   += Save;
             this.saveasbtn.Click += SaveAs;
-            this.openbtn.Click += Open;
-            this.newbtn.Click += New;
+            this.openbtn.Click   += Open;
+            this.newbtn.Click    += New;
 
             this.textboxmain.Text = string.Empty;
+
+            sidelinecoutner.Text = "1";
         }
 
         public void TextboxChange(object sender, TextChangedEventArgs e)
@@ -52,6 +56,7 @@ namespace txteditor
                     savelabel.Text = "Unsaved Changes";
                 }
 
+                // Line counter
                 int lCount = textboxmain.LineCount;
 
                 linecounterlabel.Text = $"{lCount} Line";
@@ -60,6 +65,7 @@ namespace txteditor
                 savelabel.Text = "Unsaved Changes";
                 saved = false;
 
+                // Side line counter
                 sidelinecoutner.Text = string.Empty;
                 for (int i = 1; i <= lCount; i++)
                 {
@@ -77,6 +83,7 @@ namespace txteditor
 
         private void WindowSizeChanged(object sender, SizeChangedEventArgs e)
         {
+            // Prevent textbox overflow from window
             textboxmain.Height = e.NewSize.Height - 120;
             textboxmain.Width = e.NewSize.Width - 19;
         }
@@ -114,6 +121,18 @@ namespace txteditor
 
         private void SaveAs(object sender, EventArgs e)
         {
+            var dialog = new SaveFileDialog();
+
+            bool? res = dialog.ShowDialog();
+
+            if (res == true) 
+            {
+                string filePath = dialog.FileName;
+                File.WriteAllText(filePath, textboxmain.Text);
+                currentFilePath = filePath;
+                savelabel.Text = "Saved";
+                saved = true;
+            }
         }
 
         private void Open(object sender, EventArgs e)
